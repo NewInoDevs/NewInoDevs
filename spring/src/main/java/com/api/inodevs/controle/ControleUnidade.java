14e55dbb25e5de7afa56d9419131e287c89f653e
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-05-06 15:02:53 -0300

    Finalizando Notificações

--- spring/src/main/java/com/api/inodevs/controle/ControleUnidade.java
+++ spring/src/main/java/com/api/inodevs/controle/ControleUnidade.java
@@ -55,14 +55,26 @@ public class ControleUnidade {
     }
 	
 	// Salvar a unidade e endereço editados no banco de dados ao clicar em editar:
-	@PostMapping("/salvarUnidadeEdit")
-	public String salvarUnidadeEdit(@ModelAttribute("unidade") Unidade unidade, 
-									RedirectAttributes redirect) {
-		unidade.setStatus("Pendente");
-		unidadeRepo.save(unidade);
-		redirect.addFlashAttribute("sucesso", "Unidade salvo com sucesso!");
-		return "redirect:/tabela";
-	}
+	@PostMapping("/salvarUnidadeEditRep/{id}")
+    public String salvarUnidadeEditRep(@ModelAttribute("unidade") Unidade unidade, @PathVariable("id") long id) {
+        unidade.setNotificacoes(null);
+        unidadeRepo.save(unidade);
+        notificacoesRepo.deleteById(id);
+        unidade.setStatus("Pendente");
+        Notificacoes notificacoes = new Notificacoes("ROLE_GESTOR", "Unidade");
+        unidade.setNotificacoes(notificacoes);
+        unidadeRepo.save(unidade);
+        return "redirect:/tabela";
+    }
+
+    @PostMapping("/salvarUnidadeEdit")
+    public String salvaUnidadeaEdit(@ModelAttribute("unidade") Unidade unidade, RedirectAttributes redirect) {
+        unidade.setStatus("Pendente");
+        Notificacoes notificacoes = new Notificacoes("ROLE_GESTOR", "Unidade");
+        unidade.setNotificacoes(notificacoes);
+        unidadeRepo.save(unidade);
+        return "redirect:/tabela";
+    }
 	
 	// Excluir uma unidade (junto com o seu endereço) ao clicar em excluir na tabela:
 	@GetMapping("/excluirUnidade/{cnpj}")
@@ -96,4 +108,19 @@ public class ControleUnidade {
         return "redirect:/tabela";
     }
 	
+	@PostMapping("/reprovarUnidadeRep")
+    public String reprovarUnidadeRep(@ModelAttribute("unidade") Unidade unidade) {
+        unidade.setStatus("Reprovado");
+        Notificacoes notificacoes = new Notificacoes("ROLE_DIGITADOR", "Unidade");
+        unidade.setNotificacoes(notificacoes);
+        unidadeRepo.save(unidade);
+        return "redirect:/tabela";
+    }
+	
+	@PostMapping("/aprovarUnidadeRep")
+    public String aprovarUnidadeRep(@ModelAttribute("unidade") Unidade unidade) {
+        unidade.setStatus("Aprovado");
+        unidadeRepo.save(unidade);
+        return "redirect:/tabela";
+    }
 }
