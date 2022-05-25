f5cb55034df6fe4262c3a44a3c21d0810d9c70e3
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-05-24 21:01:07 -0300

    Perfil e Média

--- spring/src/main/java/com/api/inodevs/controle/ControleTabela.java
+++ spring/src/main/java/com/api/inodevs/controle/ControleTabela.java
@@ -1,17 +1,23 @@
 package com.api.inodevs.controle;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.api.inodevs.entidades.Usuario;
 import com.api.inodevs.repositorio.ConcessionariaRepositorio;
 import com.api.inodevs.repositorio.ContaRepositorio;
 import com.api.inodevs.repositorio.ContratoRepositorio;
 import com.api.inodevs.repositorio.NotificacoesRepositorio;
 import com.api.inodevs.repositorio.RegistrosRepositorio;
 import com.api.inodevs.repositorio.UnidadeRepositorio;
+import com.api.inodevs.repositorio.UsuarioRepositorio;
 
 @Controller
 public class ControleTabela {
@@ -26,13 +32,16 @@ public class ControleTabela {
 	@Autowired
 	private UnidadeRepositorio unidadeRepo;
 	@Autowired
+	private UsuarioRepositorio usuarioRepo;
+	@Autowired
 	private NotificacoesRepositorio notificacoesRepo;
 	@Autowired
 	private RegistrosRepositorio registrosRepo;
 
 	// Entrar na página das tabelas com dados do banco de dados:
 	@GetMapping("/tabela")
-    public String tabela(Model modelo,@Param("palavraChave") String palavraChave) {
+    public String tabela(Model modelo,@Param("palavraChave") String palavraChave, @AuthenticationPrincipal User user) {
+		
 		if (palavraChave != null) {
 			modelo.addAttribute("listaConta", contaRepo.pesquisarConta(palavraChave));
 			modelo.addAttribute("listaConcessionaria", concessionariaRepo.pesquisarConcessionaria(palavraChave));
@@ -44,7 +53,9 @@ public class ControleTabela {
             modelo.addAttribute("listaUnidade", unidadeRepo.findAll());
             modelo.addAttribute("listaContrato", contratoRepo.findAll());
         }
+		
 		modelo.addAttribute("palavraChave", palavraChave);
+		
         modelo.addAttribute("quantidadeConta", notificacoesRepo.contar("Conta", "ROLE_GESTOR"));
         modelo.addAttribute("quantidadeConcessionaria", notificacoesRepo.contar("Concessionaria", "ROLE_GESTOR"));
         modelo.addAttribute("quantidadeUnidade", notificacoesRepo.contar("Unidade", "ROLE_GESTOR"));
@@ -53,6 +64,10 @@ public class ControleTabela {
         modelo.addAttribute("quantidadeConcessionariaRep", notificacoesRepo.contar("Concessionaria", "ROLE_DIGITADOR"));
         modelo.addAttribute("quantidadeUnidadeRep", notificacoesRepo.contar("Unidade", "ROLE_DIGITADOR"));
         modelo.addAttribute("quantidadeContratoRep", notificacoesRepo.contar("Contrato", "ROLE_DIGITADOR"));
+            
+        Optional<Usuario> usuarioOpt = usuarioRepo.findById(Long.parseLong(user.getUsername()));
+        modelo.addAttribute("usuarioInfo", usuarioOpt.get());
+        
         return "index";
     }
 	@GetMapping("/log")
