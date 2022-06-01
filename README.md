8f89b7909b65fb42b1097c43528d60a17c91906d
Gustavo Ando <gustavo.k.ando@gmail.com>
2022-06-01 18:04:10 -0300

    Pdf Relatorios

--- README.md
+++ README.md
@@ -1,63 +1,34 @@
-### Aprendizado por projeto integrador-FATEC-SJC
+### <p align="center">Aprendizado por projeto integrador-FATEC-SJC </center>
 
 # <p align="center"> InoDevs </center> 
 <hr>
 
 <p align="center">
-  <a href ="#dart-prosposta">  Proposta </a>  • 
-  <a href ="#pushpin-requisitos"> Requisitos </a>  • 
   <a href ="#calendar-plano-de-ação"> Plano de Ação </a>  • 
+  <a href ="#sprint-backlog">  Sprint Backlog </a>  • 
   <a href ="#computer-Wireframe"> Wireframe</a>  • 
+  <a href ="#chart_with_upwards_trend-burndown"> Burndown </a>  •
   <a href ="#rocket-tecnologias-utilizadas">  Tecnologias Utilizadas </a>  • 
-  <a href ="#mortar_board-dev-team"> Dev Team </a> • 
-
-</p>
+  <a href ="#gear-execução"> Execução(Gifs) </a> 
+</center>
 <hr>
 <br>
 
-## :dart: Prosposta
-
-Criar um sistema que ajude a TecSUS, que é responsável pela coleta e processamento de contas de energia, água e gás para diversas empresas
-dos setores do atacado e varejo, a realizar para cada conta coletada a digitação de todos campos e
-salvamento em banco de dados para eventuais consultas e análises técnicas/financeiras que podem
-trazer ao cliente oportunidades de redução de custos e alteração de contratos. 
-
-
-> [Documentação](https://github.com/NewInoDevs/NewInoDevs/blob/2ºSprint/Documentação/Inodevs3sem.pdf)
-<br>
-
-## :pushpin: Requisitos
-
-### Requisitos Funcionais
-1. &nbsp; Cadastros de Unidades, Concessionárias, Contratos;
-2. &nbsp; Cadastro do usuário e seus perfis (administrador, gestor e digitador);
-3. &nbsp; Cadastro (digitação) da conta de água, energia e gás;
-4. &nbsp; LRegistro de log de operações (cadastro e deleção);
-5. &nbsp; Relatório de consumo total de água mensal, anual e média;
-6. &nbsp; Geração de alertas de consumo acima da média (a média pode ser definida no cadastro
-do contrato).<br>
-
-
-### Requisitos Não Funcionais
-1. &nbsp; Prezar pelo UX da tela de digitação das contas;
-2. &nbsp; Incluir atalhos no teclado;
-3. &nbsp; Permitir navegação entre campos por TAB ou seta.
-<br>
  
 ## :calendar: Plano de Ação
 
 - [x] Kick-off - 14/03 a 18/03 - Concluído :heavy_check_mark:
 - [x] [Sprint 1](https://github.com/NewInoDevs/NewInoDevs/blob/1º-Sprint/README.md) - 25/03 a 14/04 - Concluído :heavy_check_mark:
 - [x] [Sprint 2](https://github.com/NewInoDevs/NewInoDevs/blob/2ºSprint/README.md) - 25/04 a 15/05 - Concluído :heavy_check_mark:
-- [x] [Sprint 3]() - 16/05 a 05/06 
+- [x] [Sprint 3](https://github.com/NewInoDevs/NewInoDevs/tree/3ºSprint) - 16/05 a 05/06 - Concluído :heavy_check_mark:
 - [x] Feira de Soluções - 15/06 
 
 
 <br>
 
-## Backlog
+## Sprint Backlog
 
-<h1 align="center"><img src = "https://github.com/NewInoDevs/NewInoDevs/blob/3ºSprint/Artefatos/backlog_3sprint.png" width="80%"></h1>
+<h1 align="center"><img src = "https://github.com/NewInoDevs/NewInoDevs/blob/3ºSprint/Artefatos/sprintbacklog3.png" width="45%"></h1>
 
 <h5 align="center">Prioridade:<br>
 🔴- Alta,
@@ -66,10 +37,10 @@ do contrato).<br>
 🔵- Seria bom ter.<br></h5>
 
 
-
 ## :computer: Wireframe
 
 Acesse o link para visualizar:
+> * versão estática disponivel na [Documentação](https://github.com/NewInoDevs/NewInoDevs/blob/2ºSprint/Documentação/Inodevs3sem.pdf)
 > * [Wireframe Figma.](https://www.figma.com/file/J7tcAZx38a8CymG7RXSG7J/Untitled?node-id=0%3A1)
   
   <br>
@@ -90,24 +61,9 @@ Acesse o link para visualizar:
 # :gear: Execução:
 
 
-
 <h1 align="center"><img src = "" width="100%" height="580px"></h1>
 
 <br>
-
-## :mortar_board: Dev Team
-
-|  Nome   |  Função |    GitHub    |    Linkedin   |
-| :---         |     :---:      |     :---:     |          :---: |
-| Júlia Maria Santos Barroso | Scrum Master  | [@GitHub](https://github.com/jumajubs) | [@LinkedIn](https://www.linkedin.com/in/j%C3%BAlia-maria-santos-850739188/) |
-| Maria Eduarda Macedo Braga | Product Owner | [@GitHub](https://github.com/madu-braga)  | [@LinkedIn](https://www.linkedin.com/in/luizhabaeb/)  |
-| Gustavo Kenji Ando | Desenvolvedor 1 | [@GitHub](https://github.com/GustavoAndo) | [@LinkedIn](https://www.linkedin.com/in/gustavo-ando-054414209/) |
-| Kauã Gustavo Rodrigues Reno | Desenvolvedor 2 | [@GitHub](https://github.com/Kaua-Reno) | [@LinkedIn](https://www.linkedin.com/in/kau%C3%A3-gustavo-r-reno-6a3142205/) 
-| Luís Henrique Ferreira Souza | Desenvolvedor 3 | [@GitHub]( https://github.com/Luisttine) | [@LinkedIn](https://www.linkedin.com/in/lu%C3%ADs-souza/) |
-| Raul Santos Iglesias | Desenvolvedor 4 | [@GitHub]( https://github.com/RaulIglesias) | [@LinkedIn](https://www.linkedin.com/in/raul-iglesias-8010201a1/) |
-
-
-
 
 <h1 align="center"></h1>
 
--- spring/src/main/java/com/api/inodevs/pdf/Pdf.java
+++ spring/src/main/java/com/api/inodevs/pdf/Pdf.java
@@ -10,6 +10,7 @@ import com.api.inodevs.entidades.Contrato;
 import com.api.inodevs.entidades.Unidade;
 import com.lowagie.text.Document;
 import com.lowagie.text.DocumentException;
+import com.lowagie.text.Element;
 import com.lowagie.text.Font;
 import com.lowagie.text.FontFactory;
 import com.lowagie.text.PageSize;
@@ -23,10 +24,10 @@ public class Pdf {
 	
     private void writeTableHeader(PdfPTable table, String[] mesesGrafico, Font fontTabela, int mes, int ano) {
         PdfPCell cell = new PdfPCell();
-        cell.setBackgroundColor(Color.BLUE);
-        cell.setPadding(8);
-         
+        cell.setBackgroundColor(Color.decode("#800080"));      
 	    cell.setPhrase(new Phrase("Contrato", fontTabela));
+    	cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
 	    table.addCell(cell);  
        
 	    int mesT = mes+1;
@@ -37,20 +38,37 @@ public class Pdf {
         		mesT-=12;
         	}
 		    cell.setPhrase(new Phrase(mesesGrafico[i]+"/\n"+anoT, fontTabela));
+	    	cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+	        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
 		    mesT++;
 		    table.addCell(cell);  
-		}
-
+        }
     }
      
-    private void writeTableData(PdfPTable table, Contrato contrato, Float[] consumosConta) {
-        table.addCell(String.valueOf(contrato.getCodigo()));
+    private void writeTableData(PdfPTable table, Contrato contrato, Float[] consumosConta, Font fontTabela, String medida) {
+    	PdfPCell cell = new PdfPCell();
+    	if (medida != null) {
+    		cell.setPhrase(new Phrase(String.valueOf(contrato.getConcessionaria().getNome()) + ":\n" + String.valueOf(contrato.getCodigo()) + " (" + medida +")", fontTabela));
+    	}
+    	else {
+    		cell.setPhrase(new Phrase(String.valueOf(contrato.getConcessionaria().getNome()) + ":\n" + String.valueOf(contrato.getCodigo()), fontTabela));
+    	}
+    	cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        table.addCell(cell);
+
         for (int i = 11; i >= 0; i--) {
         	if (consumosConta[i] == null) {
-        		table.addCell("");
-        	} else {
-        		table.addCell(String.valueOf(consumosConta[i]));
-        	}
+        		cell.setPhrase(new Phrase("", fontTabela));
+        		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        		cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        		table.addCell(cell);
+        	} else {        		
+        		cell.setPhrase(new Phrase(String.valueOf(consumosConta[i]), fontTabela));
+        		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+        		cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+        		table.addCell(cell); 		
+        	} 
 		}
     }
 	
@@ -66,46 +84,162 @@ public class Pdf {
         
         Font fontTitulo = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
         fontTitulo.setSize(18);
-        fontTitulo.setColor(Color.BLUE);
+        fontTitulo.setColor(Color.decode("#800080"));
         
         Font fontSub = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
-        fontSub.setSize(15);
+        fontSub.setSize(12);
         
         Font font = FontFactory.getFont(FontFactory.HELVETICA);
-        font.setSize(12);
+        font.setSize(11);
+        
+        Font alerta = FontFactory.getFont(FontFactory.HELVETICA);
+        alerta.setSize(11);
+        alerta.setColor(Color.RED);
+        
+        Font fontTabelaTitulo = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+        fontTabelaTitulo.setSize(12);
+        fontTabelaTitulo.setColor(Color.WHITE);
         
         Font fontTabela = FontFactory.getFont(FontFactory.HELVETICA);
         fontTabela.setSize(9);
         fontTabela.setColor(Color.WHITE);
+        
+        Font fontTabelaDado = FontFactory.getFont(FontFactory.HELVETICA);
+        fontTabelaDado.setSize(9);
          
-        Paragraph titulo = new Paragraph("Relatório de " + mes + " de " + ano, fontTitulo);
+        Paragraph titulo = new Paragraph("Relatório de " + mesExtenso + " de " + ano, fontTitulo);
         titulo.setAlignment(Paragraph.ALIGN_CENTER);
-        
-        Paragraph sub = new Paragraph("\nUnidade: " + unidade.getNome(), fontSub);
+	    document.add(titulo);
+	    
+        Paragraph sub = new Paragraph("Unidade: " + unidade.getNome(), fontSub);
+        sub.setSpacingBefore(5);
         sub.setAlignment(Paragraph.ALIGN_CENTER);
-        
-        Paragraph texto = new Paragraph("\n\nConcessionaria: " + contratoAgua.getConcessionaria().getNome()
-        		+ "\n\nConsumo: " + contaAguaAtual.getConsumo()
-        		+ "\nValor: " + contaAguaAtual.getValor_total()
-        		+ "\nMédia do Consumo Anual: " + mediaA
-        		+ "\nMédia Ideal de Consumo: " + contaAguaAtual.getMedia_consumo()
-        		+ "\n"
-        		, font);
+        document.add(sub);
         
         PdfPTable table = new PdfPTable(13);
         table.setWidthPercentage(100f);
         table.setWidths(new float[] {1f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f});
-        table.setSpacingBefore(20);
-         
+        table.setSpacingBefore(20);  
+        PdfPCell cell = new PdfPCell();
+        cell.setBackgroundColor(Color.decode("#800080"));
+        cell.setPadding(5);
+	    cell.setPhrase(new Phrase("Tabela de Consumo Mensal", fontTabelaTitulo));
+	    cell.setColspan(13);
+	    cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+	    table.addCell(cell);  
         writeTableHeader(table, mesesGrafico, fontTabela, mes, ano);
-        writeTableData(table, contratoAgua, contaAguaMes);
-        
-	    document.add(titulo);
-	    document.add(sub);
-	    document.add(texto);
+        if(contratoAgua != null) {
+        	writeTableData(table, contratoAgua, contaAguaMes, fontTabelaDado, "m³"); 
+        } 
+        if(contratoEnergia != null) {
+        	writeTableData(table, contratoEnergia, contaEnergiaMes, fontTabelaDado,"kWh");
+        } 
+        if(contratoGas != null) {
+        	writeTableData(table, contratoGas, contaGasMes, fontTabelaDado, "m³");
+        } 
         document.add(table);
         
-        document.close();
-         
+        PdfPTable tableV = new PdfPTable(13);
+        tableV.setWidthPercentage(100f);
+        tableV.setWidths(new float[] {1f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f});
+        tableV.setSpacingBefore(20);  
+        PdfPCell cellV = new PdfPCell();
+        cellV.setBackgroundColor(Color.decode("#800080"));
+        cellV.setPadding(5);
+	    cellV.setPhrase(new Phrase("Tabela de Valor Gasto Mensal (R$)", fontTabelaTitulo));
+	    cellV.setColspan(13);
+	    cellV.setHorizontalAlignment(1);
+	    tableV.addCell(cellV);  
+        writeTableHeader(tableV, mesesGrafico, fontTabela, mes, ano);
+        if(contratoAgua != null) {
+        	writeTableData(tableV, contratoAgua, gastoAguaMes, fontTabelaDado, null); 
+        } 
+        if(contratoEnergia != null) {
+        	writeTableData(tableV, contratoEnergia, gastoEnergiaMes, fontTabelaDado, null);
+        } 
+        if(contratoGas != null) {
+        	writeTableData(tableV, contratoGas, gastoGasMes, fontTabelaDado, null);
+        } 
+        document.add(tableV);
+        
+        Paragraph sub2 = new Paragraph("Dados deste mês:", fontSub);
+        sub2.setSpacingBefore(10);
+        sub2.setAlignment(Paragraph.ALIGN_CENTER);
+        document.add(sub2);
+        
+        //CONTRATO ÁGUA
+        
+        if(contratoAgua != null) {
+        	fontSub.setColor(Color.BLUE);
+        	Paragraph textoA = new Paragraph("\nConcessionária: " + contratoAgua.getConcessionaria().getNome(), fontSub);
+        	document.add(textoA);
+        	if (contaAguaAtual != null) {
+            	Paragraph texto2A = new Paragraph("Consumo: " + contaAguaAtual.getConsumo()
+                		+ "\nValor: " + contaAguaAtual.getValor_total()
+                		+ "\nMédia do Consumo Anual: " + mediaA
+                		+ "\nMédia Ideal de Consumo: " + contaAguaAtual.getMedia_consumo()
+                		, font);
+                		document.add(texto2A);
+                if (contaAguaAtual.getMedia_consumo() < mediaA) {
+                	Paragraph alertaA = new Paragraph("Consumo de Água acima da média ideal!", alerta);
+                	document.add(alertaA);
+                }
+        	}
+	        else {
+	        	Paragraph tituloNA = new Paragraph("Ainda não há uma conta de Água desse mês.");
+	        	document.add(tituloNA);
+	        } 
+        }
+        
+        //CONTRATO ENERGIA
+        
+        if(contratoEnergia != null) {
+        	fontSub.setColor(Color.RED);
+        	Paragraph textoE = new Paragraph("\nConcessionaria: " + contratoEnergia.getConcessionaria().getNome(), fontSub);
+        	document.add(textoE);
+        	if (contaEnergiaAtual != null) {
+            	Paragraph texto2E = new Paragraph("Consumo: " + contaEnergiaAtual.getConsumo()
+	        		+ "\nValor: " + contaEnergiaAtual.getValor_total()
+	        		+ "\nMédia do Consumo Anual: " + mediaE
+	        		+ "\nMédia Ideal de Consumo: " + contaEnergiaAtual.getMedia_consumo()
+	        		+ "\n"
+	        		, font);
+                	document.add(texto2E);
+               if (contaEnergiaAtual.getMedia_consumo() < mediaE) {
+                   Paragraph alertaE = new Paragraph("Consumo de Energia acima da média ideal!", alerta);
+                   document.add(alertaE);
+               }
+        	}
+	        else {
+	        	Paragraph tituloNE = new Paragraph("Ainda não há uma conta de Energia desse mês.");
+	        	document.add(tituloNE);
+	        }
+        }
+
+        //CONTRATO GAS
+        
+        if(contratoGas != null) {
+        	fontSub.setColor(Color.decode("#005000"));
+        	Paragraph textoG = new Paragraph("\nConcessionaria: " + contratoGas.getConcessionaria().getNome(), fontSub);
+        	document.add(textoG);
+        	if (contaGasAtual != null) {
+            	Paragraph texto2G = new Paragraph("Consumo: " + contaGasAtual.getConsumo()
+            		+ "\nValor: " + contaGasAtual.getValor_total()
+            		+ "\nMédia do Consumo Anual: " + mediaG
+            		+ "\nMédia Ideal de Consumo: " + contaGasAtual.getMedia_consumo()
+            		, font);
+                	document.add(texto2G);
+                if (contaGasAtual.getMedia_consumo() < mediaG) {
+                    Paragraph alertaG = new Paragraph("Consumo de Gás acima da média ideal!", alerta);
+                    document.add(alertaG);
+                }
+        	}
+	        else {
+	        	Paragraph tituloNG = new Paragraph("Ainda não há uma conta de Gás desse mês.");
+	        	document.add(tituloNG);
+	        }                         
+        }
+        
+        document.close();  
     }
 }
