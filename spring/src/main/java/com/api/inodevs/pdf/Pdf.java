98ffc5bdc1204adaccb6c16a4e58a182e0d1f782
Jonatas <jonatasmathiasdallo@gmail.com>
2022-06-01 10:29:05 -0300

    Gerar Pdf

--- spring/src/main/java/com/api/inodevs/pdf/Pdf.java
+++ spring/src/main/java/com/api/inodevs/pdf/Pdf.java
@@ -84,28 +84,93 @@ public class Pdf {
         Paragraph sub = new Paragraph("\nUnidade: " + unidade.getNome(), fontSub);
         sub.setAlignment(Paragraph.ALIGN_CENTER);
         
-        Paragraph texto = new Paragraph("\n\nConcessionaria: " + contratoAgua.getConcessionaria().getNome()
-        		+ "\n\nConsumo: " + contaAguaAtual.getConsumo()
-        		+ "\nValor: " + contaAguaAtual.getValor_total()
-        		+ "\nMédia do Consumo Anual: " + mediaA
-        		+ "\nMédia Ideal de Consumo: " + contaAguaAtual.getMedia_consumo()
-        		+ "\n"
-        		, font);
+        //CONTRATO ÁGUA
         
-        PdfPTable table = new PdfPTable(13);
-        table.setWidthPercentage(100f);
-        table.setWidths(new float[] {1f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f});
-        table.setSpacingBefore(20);
-         
-        writeTableHeader(table, mesesGrafico, fontTabela, mes, ano);
-        writeTableData(table, contratoAgua, contaAguaMes);
+        if(contaAguaAtual != null) {
+        	Paragraph textoA = new Paragraph("\n\nConcessionaria: " + contratoAgua.getConcessionaria().getNome()
+            		+ "\n\nConsumo: " + contaAguaAtual.getConsumo()
+            		+ "\nValor: " + contaAguaAtual.getValor_total()
+            		+ "\nMédia do Consumo Anual: " + mediaA
+            		+ "\nMédia Ideal de Consumo: " + contaAguaAtual.getMedia_consumo()
+            		+ "\n"
+            		, font);
+            
+            PdfPTable tableA = new PdfPTable(13);
+            tableA.setWidthPercentage(100f);
+            tableA.setWidths(new float[] {1f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f});
+            tableA.setSpacingBefore(20);
+             
+            writeTableHeader(tableA, mesesGrafico, fontTabela, mes, ano);
+            writeTableData(tableA, contratoAgua, contaAguaMes);
+            
+    	    document.add(titulo);
+    	    document.add(sub);
+    	    document.add(textoA);
+            document.add(tableA);
+        }
+        else {
+        	Paragraph tituloN = new Paragraph("\nNão existem contas de Água desse mês.");
+        	document.add(tituloN);
+        }
         
-	    document.add(titulo);
-	    document.add(sub);
-	    document.add(texto);
-        document.add(table);
         
+        //CONTRATO ENERGIA
+        
+        if(contaEnergiaAtual != null) {
+        	Paragraph textoE = new Paragraph("\n\nConcessionaria: " + contratoEnergia.getConcessionaria().getNome()
+            		+ "\n\nConsumo: " + contaEnergiaAtual.getConsumo()
+            		+ "\nValor: " + contaEnergiaAtual.getValor_total()
+            		+ "\nMédia do Consumo Anual: " + mediaE
+            		+ "\nMédia Ideal de Consumo: " + contaEnergiaAtual.getMedia_consumo()
+            		+ "\n"
+            		, font);
+            
+            PdfPTable tableE = new PdfPTable(13);
+            tableE.setWidthPercentage(100f);
+            tableE.setWidths(new float[] {1f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f});
+            tableE.setSpacingBefore(20);
+             
+            writeTableHeader(tableE, mesesGrafico, fontTabela, mes, ano);
+            writeTableData(tableE, contratoEnergia, contaEnergiaMes);
+            
+
+    	    document.add(textoE);
+            document.add(tableE);
+        }
+        
+        else {
+        	Paragraph tituloN = new Paragraph("\nNão existem contas de Energia desse mês.");
+        	document.add(tituloN);
+        }
+        //CONTRATO GAS
+        
+        if(contaGasAtual != null) {
+        	Paragraph textoG = new Paragraph("\n\nConcessionaria: " + contratoGas.getConcessionaria().getNome()
+            		+ "\n\nConsumo: " + contaGasAtual.getConsumo()
+            		+ "\nValor: " + contaGasAtual.getValor_total()
+            		+ "\nMédia do Consumo Anual: " + mediaG
+            		+ "\nMédia Ideal de Consumo: " + contaGasAtual.getMedia_consumo()
+            		+ "\n"
+            		, font);
+            
+            PdfPTable tableG = new PdfPTable(13);
+            tableG.setWidthPercentage(100f);
+            tableG.setWidths(new float[] {1f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f, 0.75f});
+            tableG.setSpacingBefore(20);
+             
+            writeTableHeader(tableG, mesesGrafico, fontTabela, mes, ano);
+            writeTableData(tableG, contratoGas, contaGasMes);
+            
+
+    	    document.add(textoG);
+            document.add(tableG);
+        }
+        
+        else {
+        	Paragraph tituloN = new Paragraph("\nNão existem contas de Gás desse mês.");
+        	document.add(tituloN);
+        }
         document.close();
-         
+        
     }
 }
